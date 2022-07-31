ad6fdae0bb14efe403a8a1dcdee6928bd3eebc51
juanl <juanl@DESKTOP-NEAV3TP>
2022-07-31 16:20:36 -0600

    Agregar archivos de proyecto.

---
+++ Hello-World2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Hello_World2
+{
+    partial class ClickThis
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.HelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(167, 105);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "hola";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // HelloWorld
+            // 
+            this.HelloWorld.AutoSize = true;
+            this.HelloWorld.Location = new System.Drawing.Point(185, 147);
+            this.HelloWorld.Name = "HelloWorld";
+            this.HelloWorld.Size = new System.Drawing.Size(35, 13);
+            this.HelloWorld.TabIndex = 1;
+            this.HelloWorld.Text = "label1";
+            // 
+            // ClickThis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.Controls.Add(this.HelloWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "ClickThis";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label HelloWorld;
+    }
+}
+
---
+++ Hello-World2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_World2
+{
+    public partial class ClickThis : Form
+    {
+        public ClickThis()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            HelloWorld.Text = "Hello World!";
+        }
+    }
+}
